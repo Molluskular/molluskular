7b2e6a099cb2b0077b8ae72fd22da95b0ad3d56c
beming-dev <mingfordev@gmail.com>
2023-11-10 11:59:27 +0100

    Add hello page

---
+++ src/app/hello/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+export default function Home() {
+  return (
+    <>
+      <main className="test">
+        <div className="test2">Test2</div>
+      </main>
+      <style jsx>{`
+        .test {
+          .test2 {
+            font-size: 100px;
+          }
+        }
+      `}</style>
+    </>
+  );
+}
