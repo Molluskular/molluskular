3ad72c0fe8266edaff1a961195267ee3afd7a0ca
beming-dev <mingfordev@gmail.com>
2023-11-13 09:52:32 +0100

    Add home page

---
+++ src/app/Concept.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Concept() {
+  return (
+    <div className="page pg-02">
+      <span>OUR CONCEPT</span>
+      <div className="concepts"></div>
+    </div>
+  );
+}
